95a8759d632977d48085da2c4bd6255cf42e070a
audtheshaud <abookernelson@gmail.com>
2025-01-14 18:05:43 -0500

    left comments

--- server.c
+++ server.c
@@ -12,33 +12,38 @@
 #include <string.h>     // String definitions
 #include <pthread.h>    // Posix Threads
 
-#define PORT 5555
-#define THREADCOUNT 5
+#define PORT 5555        // Arbitrary port
+#define MAXTHREADCOUNT 5 // Maximum thread count
 
-struct args{
-    int client_sd;
-    char server_msg[128];
-    int tid;
+struct args
+{                         // Struct of args for each thread
+    int client_sd;        // Client socket descriptor
+    char server_msg[128]; // Server message
+    int tid;              // Thread id
 };
 
-void* handle_client(void* args){
-    struct args* arguments = args;
-    if ((send(arguments->client_sd, arguments->server_msg, strlen(arguments->server_msg),0)) < 0){
+void *handle_client(void *args)
+{                                  // Thread that handles each client by sending a server message and closing the connection
+    struct args *arguments = args; // Set struct of type struct void* to struct* args
+    if ((send(arguments->client_sd, arguments->server_msg, strlen(arguments->server_msg), 0)) < 0)
+    {                                                              // Send the Server message using the Client's socket descriptor
         printf("Failed to send on socket: %s\n", strerror(errno)); // If sending fails then exit the program
         exit(0);
     }
-    shutdown(arguments->client_sd, SHUT_RDWR);
-    close(arguments->client_sd);
-    free(arguments);
-    return NULL;
+    shutdown(arguments->client_sd, SHUT_RDWR); // Shutdown the Client's socket descriptor to prevent reading and writing
+    close(arguments->client_sd);               // Close the Client's socket descriptor
+    free(arguments);                           // Free the arguments struct, requires the pointer to the struct
+    return NULL;                               // Return from the thread
 }
 
-int main() {
+int main()
+{
     int num;
     printf("Enter a number between 1-100: "); // Prompt user for random number between 1-100
-    while ((scanf("%3d", &num) != 1) || num < 1 ||  num > 100){ // Limiting scanf input to max of 3 characters while checking num is between 1 and 100
+    while ((scanf("%3d", &num) != 1) || num < 1 || num > 100)
+    { // Limiting scanf input to max of 3 characters while checking num is between 1 and 100
         printf("Please enter a number between 1-100...\n");
-    } 
+    }
     printf("Your number is %d\n", num); // Print the user's chosen number
 
     char str[4];
@@ -46,13 +51,14 @@ int main() {
     char server_name[] = "Aud's Server: #";
     char server_message[128];
     strcpy(server_message, server_name); // Copy the server name into the message
-    strcat(server_message, str); // Concatenate the server name and the number
+    strcat(server_message, str);         // Concatenate the server name and the number
     printf("Server message: %s\n", server_message);
 
-    int server_descriptor; // Socket file descriptor
+    int server_descriptor;          // Socket file descriptor
     struct sockaddr_in server_addr; // Server socket address information for IPv4 addresses
 
-    if ((server_descriptor = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0){ // Create socket that uses the IPv4 address family, Reliable Byte-stream, and TCP
+    if ((server_descriptor = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    {                                                                 // Create socket that uses the IPv4 address family, Reliable Byte-stream, and TCP
         printf("Failed to create TCP socket: %s\n", strerror(errno)); // If creating the socket fails then exit the program
         exit(0);
     }
@@ -60,61 +66,71 @@ int main() {
     int opt = 1;
     setsockopt(server_descriptor, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    server_addr.sin_family = AF_INET; // Set address family to IPv4
-    server_addr.sin_port = htons(PORT); // Set port using Host to Network Short
+    server_addr.sin_family = AF_INET;                     // Set address family to IPv4
+    server_addr.sin_port = htons(PORT);                   // Set port using Host to Network Short
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Allow any IP addresses available to the Host to connect
-    
-    if ((bind(server_descriptor, (struct sockaddr *)&server_addr, sizeof(server_addr))) < 0){ // Bind the socket address (IPv4 + Port)
+
+    if ((bind(server_descriptor, (struct sockaddr *)&server_addr, sizeof(server_addr))) < 0)
+    {                                                           // Bind the socket address (IPv4 + Port)
         printf("Failed to bind socket: %s\n", strerror(errno)); // If binding the socket fails then exit the program
         exit(0);
     }
-    
-    if ((listen(server_descriptor, 10)) < 0){ // Listen for one client using Socket Descriptor
+
+    if ((listen(server_descriptor, 10)) < 0)
+    {                                                                // Listen for one client using Socket Descriptor
         printf("Failed to listen on socket: %s\n", strerror(errno)); // If listening fails then exit the program
         exit(0);
-    } else{
+    }
+    else
+    {
         printf("Listening...\n");
     }
-    int client_descriptor;
-    struct sockaddr_in client_addr;
-    memset(&client_addr, 0, sizeof(client_addr));
-    socklen_t client_length = sizeof(client_addr);
-    
-    int client_count = -1;
-    pthread_t threads[THREADCOUNT];
+    int client_descriptor;                         // Create a Client socket descriptor
+    struct sockaddr_in client_addr;                // Create a socket address IPv4 struct for the Client
+    memset(&client_addr, 0, sizeof(client_addr));  // Initialize the Client socket address IPv4 struct to 0
+    socklen_t client_length = sizeof(client_addr); // Get the length of the Client socket address IPv4 struct
+
+    int client_count = -1;             // Set the client_count
+    pthread_t threads[MAXTHREADCOUNT]; // Create a pool of threads
     struct args thread_args;
 
-    while(1){
-        if (client_count == THREADCOUNT){
-            //printf("Max number of clients has been reached, please wait for another client to disconnect...\n");
+    while (1)
+    { // Thread initilization loop, creates and detaches threads for each Client connection
+        if (client_count == MAXTHREADCOUNT)
+        {
+            // printf("Max number of clients has been reached, please wait for another client to disconnect...\n");
             break;
-        }else{
-            if ((client_descriptor = accept(server_descriptor, (struct sockaddr *)&client_addr, &client_length)) < 0){
-            printf("Failed to accept on socket: %s\n", strerror(errno)); // If accepting fails then exit the program
-            exit(0);
+        }
+        else
+        {
+            if ((client_descriptor = accept(server_descriptor, (struct sockaddr *)&client_addr, &client_length)) < 0)
+            {
+                printf("Failed to accept on socket: %s\n", strerror(errno)); // If accepting fails then exit the program
+                exit(0);
             }
-            char client_ip[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN); // IPv4 Network to Presentation
-            printf("Successfully connected to client: %s:%d\n", client_ip,ntohs(client_addr.sin_port));
-            client_count += 1;
-            printf("Clients connected: %d\n\n", client_count+1);
+            char client_ip[INET_ADDRSTRLEN];                                                             // Create a buffer to get the Client's IP Address
+            inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);                       // IPv4 Network to Presentation
+            printf("Successfully connected to client: %s:%d\n", client_ip, ntohs(client_addr.sin_port)); // Print confirmtion of the Client's connection
+            client_count += 1;                                                                           // Increase the Client count
+            printf("Clients connected: %d\n\n", client_count + 1);                                       // Print the amount of connected clients
 
-            struct args* thread_args = malloc(sizeof(struct args));
-            thread_args->client_sd = client_descriptor;
-            memset(thread_args->server_msg, 0, sizeof(thread_args->server_msg));
-            strcpy(thread_args->server_msg, server_message);
+            struct args *thread_args = malloc(sizeof(struct args));              // Dynamically allocate a Thread Argument Struct
+            thread_args->client_sd = client_descriptor;                          // Save the Client Socket Descriptor to the Struct
+            memset(thread_args->server_msg, 0, sizeof(thread_args->server_msg)); // Initialize the Server message to 0
+            strcpy(thread_args->server_msg, server_message);                     // Save the server message to the Struct
             thread_args->tid = client_count;
 
-            if ((pthread_create(&threads[client_count], NULL, handle_client, (void *)thread_args)) != 0){
+            if ((pthread_create(&threads[client_count], NULL, handle_client, (void *)thread_args)) != 0) // Create the thread
+            {
                 printf("Failed to create thread for client: %s\n", strerror(errno));
                 exit(0);
             }
-            if (pthread_detach(threads[client_count]) != 0){
+            if (pthread_detach(threads[client_count]) != 0) // Detach the thread to prevent Clients from terminating other Clients
+            {
                 printf("Failed to detach thread for client: %s\n", strerror(errno));
                 exit(0);
             }
         }
-
     }
     printf("All threads have been utilized, powering off the server...\n");
     sleep(5);
@@ -122,7 +138,6 @@ int main() {
     pthread_exit(NULL);
     return 0;
 }
-
 
 /*
 char thread_id[2];
